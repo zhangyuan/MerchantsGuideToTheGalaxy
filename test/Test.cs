85c1ccd082a1d091cc2bad2a1e7419fe37d9c0e7
Yuan Cheung <evyuancheung@gmail.com>
2014-07-26 23:53:30 +0800

    [evzhang] tasks for main questions.

--- test/Test.cs
+++ test/Test.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xunit;
 
 namespace test
 {
     public class Test
     {
+        [Fact]
+        public void should_calculate_intergalactic_numbers()
+        {
+            
+        }
+
+        [Fact]
+        public void should_calculate_total_price_of_intergalactic_goods()
+        {
+            
+        }
+
+        [Fact]
+        public void should_return_error_message_with_invalid_question()
+        {
+            
+        }
     }
 }
