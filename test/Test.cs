2324e48ba7d2356b00ed7be057bf6d63d87da292
Yuan Cheung <evyuancheung@gmail.com>
2014-07-27 17:03:54 +0800

    [evzhang] add test for all inputs

--- test/Test.cs
+++ test/Test.cs
@@ -67,5 +67,21 @@ namespace test
             progam.AddInput("glob is I\r\nprok is V\r\npish is X\r\ntegj is L\r\nhow much wood could a woodchuck chuck if a woodchuck could chuck wood ?");
             Assert.Equal("I have no idea what you are talking about", progam.GetOutputText());
         }
+
+        [Fact]
+        public void should_return_all_outputs_for_all_inputs()
+        {
+            var progam = new MyProgram();
+            var input = "glob is I\r\nprok is V\r\npish is X\r\ntegj is L\r\nglob glob Silver is 34 Credits\r\n" +
+                        "glob prok Gold is 57800 Credits\r\npish pish Iron is 3910 Credits\r\n" +
+                        "how much is pish tegj glob glob ?\r\nhow many Credits is glob prok Silver ?\r\n" +
+                        "how many Credits is glob prok Gold ?\r\nhow many Credits is glob prok Iron ?\r\n" +
+                        "how much wood could a woodchuck chuck if a woodchuck could chuck wood ?";
+            var expected = "pish tegj glob glob is 42\r\nglob prok Silver is 68 Credits\r\n" +
+                           "glob prok Gold is 57800 Credits\r\nglob prok Iron is 782 Credits\r\n" +
+                           "I have no idea what you are talking about";
+            progam.AddInput(input);
+            Assert.Equal(expected, progam.GetOutputText());
+        }
     }
 }
